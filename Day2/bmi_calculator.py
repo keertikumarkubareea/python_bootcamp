7b492e16b475189a71c23c4f717328d0c3e82e29
keertikumarkubareea <keertikumarkubareea@gmail.com>
2024-04-03 17:30:17 -0400

    day 2: BMI calculator

---
+++ Day2/bmi_calculator.py
@@ -0,0 +1,23 @@
+"""
+Author: Keertikumar Kubareea
+Project: BMI calculator
+"""
+
+
+def calc_bmi(weight: float, height: float) -> int:
+    bmi = int(weight / (height ** height))
+    return bmi
+
+
+weight = float(input("What's your weight in kilograms(kg)?: "))
+height = float(input("What is your height in metres(m)?: "))
+bmi = calc_bmi(weight=weight, height=height)
+print("Your BMI is equal to " + str(bmi))
+if bmi < 18.5:
+    print("You are underweight")
+elif bmi < 24.9:
+    print("You are healthy")
+elif bmi < 29.9:
+    print("You are overweight")
+else:
+    print("You are obese")
