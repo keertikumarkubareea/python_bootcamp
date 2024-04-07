21d3bcb55029c6b5985b31e7489c669096a9c7ce
keertikumarkubareea <keertikumarkubareea@gmail.com>
2024-04-06 23:21:00 -0400

    day 3

--- Day3/rollercoaster.py
+++ Day3/rollercoaster.py
@@ -17,11 +17,19 @@ h = float(input("Please enter your height (in cm): "))
 if can_ride(h):
     print("You can ride the rollercoaster!")
     age = int(input("Please input your age: "))
+    price = 0
     if age <= 12:  # Ages between 0 - 12 incl.
-        print("Please pay $5.")
+        print("Child tickets are $5.")
+        price = 5
     elif age <= 18:  # Ages between 13 - 18 incl.
-        print("Please pay $7.")
+        print("Youth tickets are $7.")
+        price = 7
     else:  # Ages above 18
-        print("Please pay $12.")
+        print("Adult tickets are $12.")
+        price = 12
+    pics = input("Do you want photos? (y or n): ")
+    if pics == "y" or pics == "Y":
+        price += 3
+    print (f"Your total payment due is ${price}")
 else:
     print("You need to grow taller before you can ride the rollercoaster")
