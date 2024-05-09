fbb3e032d5a70f60fa19c72bf29efc226e6ddc01
keertikumarkubareea <keertikumarkubareea@gmail.com>
2024-05-08 22:12:02 -0400

    added additional explanation in the caesar program

--- Day8/caesar_cipher.py
+++ Day8/caesar_cipher.py
@@ -1,5 +1,9 @@
 """
 Day 8: Final capstone project - building a Caesar Cipher to encrypt and decrypt text
+
+Additional practice: modify the encrypt and the decrypt functions into a single function that takes a 3rd argument
+'direction' that determines whether it is an encrypt or a decrypt
+
 """
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
