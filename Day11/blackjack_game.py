1d829bab0422dd7d2f1b93f9bf5f546fbb14e9f4
keertikumarkubareea <keertikumarkubareea@gmail.com>
2024-05-12 11:06:06 -0400

    completed the game program flow in the main method

--- Day11/blackjack_game.py
+++ Day11/blackjack_game.py
@@ -75,6 +75,79 @@ def deal_one_hand(current_deck: dict, current_hand: dict) -> tuple:
 def main():
     # TODO
     print("Welcome to a game of Blackjack!!")
+    print("----------------------------------------------------")
+    ready_to_play = ""
+    while ready_to_play.lower() != "yes":
+        ready_to_play = input("Is the player ready to play? ('yes'/'no'): ")
+    deck = common_constants.START_DECK
+    print("The deck of cards has been shuffled . . .")
+    player = {}
+    dealer = {}
+    deck, player, dealer = deal_hands(current_deck=deck, user_hand=player, dealer_hand=dealer)
+    initial_dealer_score = calc_hand_total(hand=dealer)
+    player_score = calc_hand_total(hand=player)
+    print("Show of first card:")
+    print(f"The dealer has the following cards {dealer} with the following score: {initial_dealer_score}")
+    print(f"The player has the following cards {player} with the following score: {player_score}")
+    print("The first hands have been distributed and revealed.")
+    print("---------------------------------------------------")
+    dealer_score = initial_dealer_score
+    game_on = True
+    while game_on:
+        print(f"Your score is {player_score}")
+        player_draw = ""
+        while player_draw != "yes" and player_draw != "no":
+            print("Please enter 'yes' or 'no'. . .")
+            player_draw = input("Would you like to draw a card from the deck? ('HIT'): ")
+        if player_draw == "no":
+            print("Player chose to STAND and not take a card")
+            game_on = False
+        else:
+            deck, player = deal_one_hand(current_deck=deck, current_hand=player)
+            player_score = calc_hand_total(hand=player)
+            print(f"Player has the following hand: {player}")
+            print(f"Player has the following score: {player_score}")
+            if player_score > 21:
+                # Player Bust - will be printed outside the loop
+                game_on = False
+                # Immediate loss of Player - break out of the loop
+                continue
+        if dealer_score < 21:
+            deck, dealer = deal_one_hand(current_deck=deck, current_hand=dealer)
+            dealer_score = calc_hand_total(dealer)
+            print(f"Dealer has acquired a card (hidden)")
+            # calculating the number of cards that the dealer has
+            dealer_cards = 0
+            for card in dealer:
+                dealer_cards += dealer[card]
+            print(f"Dealer has the score of {initial_dealer_score} + the score from {dealer_cards - 1} more cards")
+            if dealer_score > 21:
+                # Dealer Bust - will be printed outside the loop
+                game_on = False
+                # Immediate loss of Dealer - break out of the loop
+                continue
+
+    print("----------------------------------------------------")
+    print(f"Dealer hand: {dealer}")
+    print(f"Dealer score: {dealer_score}")
+    print("----------------------------------------------------")
+    print(f"Player hand: {player}")
+    print(f"Player score: {player_score}")
+    if dealer_score > 21:
+        print("Dealer BUSTED")
+        print("Player wins!!")
+    elif player_score > 21:
+        print("Player BUSTED")
+        print("Dealer wins!!")
+    # No one is busted from here on
+    elif player_score == dealer_score:
+        print("DRAW")
+    elif player_score > dealer_score:
+        print(f"Player wins with a score of {player_score}")
+        print(f"Dealer loses with {dealer_score} points")
+    else:
+        print(f"Dealer wins with a score of {dealer_score}")
+        print(f"Player loses with {player_score} points")
 
 
 if __name__ == "__main__":
