bee9cf9879e3be19a74db147e9d9e0609aee08c6
keertikumarkubareea <keertikumarkubareea@gmail.com>
2024-05-11 22:29:06 -0400

    Started the blackjack game with helper functions and constant values

---
+++ Day11/blackjack_game.py
@@ -0,0 +1,55 @@
+"""
+The game of blackjack - in Python - Capstone project
+"""
+
+import random
+import common_constants
+
+
+def calc_hand_total(hand: dict) -> int:
+    """
+    Each hand is represented as a dictionary in the following format:
+    {card: count}
+    For example {TWO: 2, ACE: 1}
+    means that the user has 2 cards of TWO and 1 Ace card
+    Total = 2 + 2 + 11 = 15
+    """
+    total = 0
+    for card in hand:
+        if (card == common_constants.KING_CARD or card == common_constants.QUEEN_CARD or
+                card == common_constants.JACK_CARD):
+            total += 10 * hand[card]
+        elif card == common_constants.ACE_CARD:
+            if total > 10:
+                total += 1 * hand[card]
+            else:
+                total += 11 * hand[card]
+        else:
+            total += int(card) * hand[card]
+    return total
+
+
+def get_card_from_deck(current_deck: dict) -> tuple:
+    random_card = random.choice(list(current_deck.keys()))
+    # decrement the card count in the deck by 1
+    current_deck[random_card] -= 1
+    if current_deck[random_card] <= 0:
+        current_deck.pop(random_card)
+    return random_card, current_deck
+
+
+def add_card_to_hand(current_hand: dict, card: str) -> dict:
+    if card in current_hand:
+        current_hand[card] += 1
+    else:
+        current_hand[card] = 1
+    return current_hand
+
+
+def main():
+    # TODO
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Day11/common_constants.py
@@ -0,0 +1,33 @@
+"""
+Constants file for strings to be used in the Blackjack capstone project
+"""
+
+ACE_CARD = "ace"
+KING_CARD = "king"
+QUEEN_CARD = "queen"
+JACK_CARD = "jack"
+TWO = "2"
+THREE = "3"
+FOUR = "4"
+FIVE = "5"
+SIX = "6"
+SEVEN = "7"
+EIGHT = "8"
+NINE = "9"
+TEN = "10"
+START_DECK_COUNT = 52
+START_DECK = {
+    ACE_CARD: 4,
+    KING_CARD: 4,
+    QUEEN_CARD: 4,
+    JACK_CARD: 4,
+    TWO: 4,
+    THREE: 4,
+    FOUR: 4,
+    FIVE: 4,
+    SIX: 4,
+    SEVEN: 4,
+    EIGHT: 4,
+    NINE: 4,
+    TEN: 4
+}
