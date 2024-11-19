3a58c7a647e28ac3f11cc0f9fee47fdf27a9ab09
keertikumarkubareea <keertikumarkubareea@gmail.com>
2024-11-18 22:10:11 -0500

    skipping day 22 to move to next implementation

---
+++ Day22/pong_game.py
@@ -0,0 +1,17 @@
+from turtle import Screen
+
+'''
+TODO: Skipping the GUI training for now
+'''
+
+
+def main():
+    screen = Screen()
+    # The code below sets the screen to full size
+    screen.setup(width=1.0, height=1.0)
+
+    screen.exitonclick()
+
+
+if __name__ == "__main__":
+    main()
