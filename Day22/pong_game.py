72a39580ce6ae0e85383dc91587e2851ac6981eb
keertikumarkubareea <keertikumarkubareea@gmail.com>
2025-01-07 19:07:24 -0500

    started the pong game

--- Day22/pong_game.py
+++ Day22/pong_game.py
@@ -1,14 +1,26 @@
-from turtle import Screen
-
-'''
-TODO: Skipping the GUI training for now
-'''
+from turtle import Screen, Turtle
+from player_paddle import PlayerPaddle
 
 
 def main():
+    # Setting up the screen
     screen = Screen()
-    # The code below sets the screen to full size
-    screen.setup(width=1.0, height=1.0)
+    screen.bgcolor("black")
+    screen.setup(width=800, height=600)
+    screen.title("Pong")
+    screen.tracer(0)
+
+    # Create and move a paddle
+    paddle = PlayerPaddle()
+
+    screen.update()  # Update screen once all the objects have been initialized and move to their respective places
+    screen.tracer(1)  # Turn on tracer for the game
+
+    # to move the paddle with keystrokes, get the screen to listen
+    screen.listen()
+    # when using a function as parameter, do not add parentheses
+    screen.onkey(paddle.go_up, "Up")
+    screen.onkey(paddle.go_down, "Down")
 
     screen.exitonclick()
 
